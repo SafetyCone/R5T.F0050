34f44608f0a1f05c920afba818f4e00b9c24af96
davidcoats <david@getsafetycone.com>
2024-08-27 22:53:22 -0700

    Interim commit.

--- source/R5T.F0050/Code/Values/IDirectoryNames.cs
+++ source/R5T.F0050/Code/Values/IDirectoryNames.cs
@@ -8,6 +8,9 @@ namespace R5T.F0050
 	[ValuesMarker]
 	public partial interface IDirectoryNames : IValuesMarker
 	{
-
+        /// <summary>
+        /// <para><value>Source</value></para>
+        /// </summary>
+        public string Source => "Source";
     }
 }
